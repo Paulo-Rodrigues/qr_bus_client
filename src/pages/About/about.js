f88b269e0127832d4571f25765070cfb124d2cee
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2022-06-20 22:36:17 -0300

    add about

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -1,5 +1,28 @@
 import styled from 'styled-components';
 
 export const StyledAbout = styled.section`
-  background-color: red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
+  justify-content: center;
 `
+export const StyledCard = styled.div`
+  padding: 1rem;
+  width: 70%;
+  border: 1px solid #fff;
+  border-radius: 5px;
+  background-color: #333;
+  color: #fff;
+
+  h1, h2 {
+    text-align: center;
+    padding: 10px;
+    border-bottom: 1px solid #fff;
+  }
+
+  p {
+    text-align: center;
+  }
+`;
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyledAbout } from './about';
+import { StyledAbout, StyledCard } from './about';
 
 function About() {
   return (
     <>
       <StyledAbout>
-        <h1>About Page</h1>
+        <StyledCard>
+          <h1>Sobre o App</h1>
+          <p>QR Bus é um aplicativo desenvolvido para permitir a consulta de linhas
+          e trajetos do transporte urbano do município de Pompéia</p>
+        </StyledCard>
+        <StyledCard>
+          <h2>Equipe de desenvolvimento</h2>
+          <p>Gilmar<br/>Letícia<br/>Carolina<br/>Carlos<br/>Anderson<br/>Paulo</p>
+        </StyledCard>
       </StyledAbout>
     </>
   )
